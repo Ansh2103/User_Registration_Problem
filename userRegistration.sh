c262bcfe72a044d54d361203fd20bca92ddf90e1
Ansh2103 <swayamshubham007.sm@gmail.com>
2020-07-07 21:23:48 +0530

    Added : First Name Start With Capital letter And Has Minimum Three Letters

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,24 @@
 #!/bin/bash -x
+shopt -s extglob
+#function to take firstname as an input
+function input()
+{
+ echo "Enter the FirstName"
+ read name
+ firstName $name
+}
 
-echo "Welcome User"
+#function to validate firstname with first letter being capital followed by minimum 3 characters
+function firstName
+{
+ word=$1
+ firstNameExpression="^[A-Z]{1}[a-z]{2,}$"
+ if [[ $word =~ $firstNameExpression ]]
+ then
+  echo Valid Name;
+ else
+  echo Invalid Name;
+ fi
+}
+
+input
